082597526051c22626d66e78bcbdc0cb1a538ed4
Asa Adomatis <asaa@clemson.edu>
2025-04-10 17:28:03 -0400

    Final Updates before culling

--- pola_tools.py
+++ pola_tools.py
@@ -25,6 +25,9 @@ from dotenv import load_dotenv
 import re
 import json
 
+# other imports
+import pandas as pd
+
 class CardClassificationOutputParser(BaseOutputParser):
     def parse(self, text: str):
 
@@ -52,29 +55,23 @@ class PolaTool():
 
         self.gemini_model = gemini_model
 
-    def classify_trainers(self, cards:list[Card], batch_size:int=25):
+    def classify_trainers(self, cards:list[Card]):
         """ Given a list of trainer cards from the PokemonTCG SDK, returns a list of classifications of those trainers"""
-        card_texts = []
 
         # getting all card text into a list
+        results = []
         for card in cards:
             if card.supertype != "Trainer":
                 raise ValueError(f"PolaTool::classify_trainers: {card.name} is not a Trainer!")
 
+            # extracting card text
             if len(card.rules) == 2:
                 card_text = card.rules = card.rules[0]
             else:
                 card_text = " ".join(card.rules[:-1])
-            card_texts.append(card_text)
 
-        # split into batches
-        batches = [card_texts[i:i + batch_size] for i in range(0, len(card_texts), batch_size)]
-        
-        results = []
-        for batch in batches:
-            #results.append(batch)
             prompt = PromptTemplate.from_template("""
-                You are a Pokemon TCG expert and further an expert at designing Pokemon TCG cubes. Given a list of effect texts of a Trainer cards, classify each of them as either
+                You are a Pokemon TCG expert and further an expert at designing Pokemon TCG cubes. Given the card effect text of a Pokemon Trainer card, classify it as either:
                                     
                 1. **Utility** - cards that provide specific situational value and do not provided any boost to a decks consistency.
                 2. **Concistency** - cards that improve the chances of executing a strategy. Most consistency cards will draw you more cards, search your deck for a card, or let you select from a few. Consistency is further classified into:
@@ -111,10 +108,10 @@ class PolaTool():
                 Card Text: "Draw 3 cards. Switch out your opponent's Active Pokémon to the Bench. (Your opponent chooses the new Active Pokémon.)"
                 Output: "category: "Both", "subtype": "Draw"
 
-                ## Now classify the following list:       
-                Card Text: "{card_texts}"
+                ## Now classify the following card:       
+                Card Text: "{card_text}"
                                     
-                Return a list of json object like this:
+                Return a json object like this:
                 "category": "Utility" or "Consistency", "subtype": "Draw" or "Search" or "Pick" or null
                 """
             )
@@ -126,7 +123,95 @@ class PolaTool():
                 output_parser=CardClassificationOutputParser()
             )
 
-            result = classification_chain.run(card_texts=batch)
-            results.extend(result)
+            result = classification_chain.run(card_text=card_text)
+            results.append(result)
 
         return results
+    
+def cards_to_df(cards:list[Card]) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    # add cards to dataframe
+    card_dict = {
+        "id": [],
+        "name": [],
+        "supertype": [],
+        "subtypes": [], # must be converted to string for storage
+        "hp": [],
+        "type": [], # same here
+        "evolvesFrom": [],
+        "weakness_types": [],
+        "weakness_values": [],
+        "convertedRetreatCost": [],
+        "set_id": [],
+        "number": [],
+        "artist": [],
+        "rarity": [],
+        "flavorText": [],
+        "small_image_url": [], # dealt with differently
+        "large_image_url": [] # dealt with differently
+    }
+
+    abilities_dict = {
+        "card_id": [],
+        "name": [],
+        "text": [],
+        "type": []
+    }
+
+    attack_dict ={
+        "card_id": [],
+        "name": [],
+        "text": [],
+        "cost": [], # same here
+        "convertedEnergyCost": [],
+        "damage": []
+    }
+
+    # converting cards into dictonaries
+    for card in cards:
+        if card is not None:
+            card_dict["id"].append(card.id)
+            card_dict["name"].append(card.name)
+            card_dict["supertype"].append(card.supertype)
+            card_dict["subtypes"].append(', '.join(card.subtypes) if card.subtypes else None)
+            card_dict["hp"].append(card.hp)
+            card_dict["type"].append(', '.join(card.types) if card.types else None)
+            card_dict["evolvesFrom"].append(card.evolvesFrom)
+
+            # dealing with special case weaknesses
+            weakness_types = [weakness.type for weakness in card.weaknesses] if card.weaknesses else None
+            weakness_values = [weakness.value for weakness in card.weaknesses] if card.weaknesses else None
+            card_dict["weakness_types"].append(', '.join(weakness_types) if weakness_types else None)
+            card_dict["weakness_values"].append(', '.join(weakness_values) if weakness_values else None)
+
+            card_dict["convertedRetreatCost"].append(card.convertedRetreatCost)
+            card_dict["set_id"].append(card.set.id)
+            card_dict["number"].append(card.number)
+            card_dict["artist"].append(card.artist)
+            card_dict["rarity"].append(card.rarity)
+            card_dict["flavorText"].append(card.flavorText)
+            card_dict["small_image_url"].append(card.images.small)
+            card_dict["large_image_url"].append(card.images.large)
+
+            # add abilities to abilities_dict
+            if card.abilities:
+                for ability in card.abilities:
+                    abilities_dict["card_id"].append(card.id)
+                    abilities_dict["name"].append(ability.name)
+                    abilities_dict["text"].append(ability.text)
+                    abilities_dict["type"].append(ability.type)
+            
+            # add attacks to attack_dict
+            if card.attacks:
+                for attack in card.attacks:
+                    attack_dict["card_id"].append(card.id)
+                    attack_dict["name"].append(attack.name)
+                    attack_dict["text"].append(attack.text)
+                    attack_dict["cost"].append(', '.join(attack.cost) if attack.cost else None)
+                    attack_dict["convertedEnergyCost"].append(attack.convertedEnergyCost)
+                    attack_dict["damage"].append(attack.damage)
+
+    # converting to dataframes and displaying them
+    cards_df = pd.DataFrame(card_dict)
+    abilities_df = pd.DataFrame(abilities_dict)
+    attacks_df = pd.DataFrame(attack_dict)
+    return cards_df, abilities_df, attacks_df
